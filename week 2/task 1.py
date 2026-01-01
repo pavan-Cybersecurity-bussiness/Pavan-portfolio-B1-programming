636c06fa8b22ae50b29239bdc58042fdcec60f37
Pavan Ashokkumar Krishnaveni <akpavan@pavans-mbp.speedport.ip>
2026-01-01 22:45:17 +0100

    week 2 update

--- week 2/task 1.py
+++ week 2/task 1.py
@@ -1,5 +1,5 @@
 print("installed python")
-print("current python version : 3.12.4")
+print("current python version : 3.12.4 ")
 print("While Jupyter Notebook is useful in many contexts, "
       "\n I prefer using PyCharm for this task because it aligns better with my development workflow.")
 print("created a github account")
