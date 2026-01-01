40e8ecb6f91e8aea48232723d474baa31ec059e8
Pavan Ashokkumar Krishnaveni <akpavan@pavans-mbp.speedport.ip>
2026-01-01 22:43:02 +0100

    week 1 updated

---
+++ week 2/task 2.py
@@ -0,0 +1,20 @@
+print("step 1")
+# My apologies for not reading the task earlier the current file name is Pavan-portfolio-B1-programming
+# unfortunately  I can't change the name of the repo and update it in the mooodle
+print("current repo name : Pavan-portfolio-B1-programming")
+print("added read me file")
+print("Completed step 1")
+
+print("------------")
+print("step 2")
+print(" github for desktop for mac was downloaded and installed")
+print("Completed step 2")
+
+print("------------")
+print("step 3")
+print("completed cloning the repo")
+print("Completed step 3")
+
+print("------------")
+print("Task completed")
+print("-*-*-*-*-*-*")
---
+++ week 2/task 3.py
@@ -0,0 +1,24 @@
+import random
+#1
+
+a =1.12233 #float
+b =2111 #int
+print("a is a : ",type(a), "b is a :", type(b))
+print("this is a string")
+
+#2
+numb1 = random.randrange(10000)
+print("number 1 is ", numb1)
+numb2 = random.randrange(10000)
+print("number 2 is ", numb2)
+print("add")
+print(numb1 + numb2 )
+print("subtract")
+print(numb1 - numb2 )
+print("multiple")
+print(numb1 * numb2 )
+print("divide")
+print(numb1 / numb2 )
+
+#3
+#stored and reuse variables throught the program
